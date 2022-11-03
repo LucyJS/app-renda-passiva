7561239220aa83eff1c9022fc1c2a47c9e2d59b8
JoneyJS <joneyjavascript@gmail.com>
2022-11-03 09:42:53 -0300

    refatoração: autocarregamento de javascript e css apenas pelo nome do component em autoload.js

---
+++ utils/autoload.js
@@ -0,0 +1,86 @@
+
+const components = [
+    "acoes",
+    "botao-personalizado",
+    "historico-transacao",
+    "lista",
+    "notificacao",
+    "receitas",
+    "resumo",
+    "selecionar-variacao"
+]
+
+const constants = [
+    "financial-movement-type",
+    "stock-ticket",
+    "stock-variation",
+]
+
+const classes = [
+    "stock"
+]
+
+const utils = [
+    "util"
+]
+
+loadScriptList("./utils", utils);
+loadScriptList("./class", classes);
+loadScriptList("./constants", constants);
+setTimeout(() => {
+    loadComponentList(components);
+    loadScript("index.js");
+    loadCss("index.css");
+}, 0);
+
+function loadScriptList(path, list){
+    list.forEach(name => {
+        loadScript(`${path}/${name}.js`);
+    })
+}
+
+function loadComponentList(componentList){
+    componentList.forEach(componentName => {
+        loadComponent(componentName);
+    })
+}
+
+function loadComponent(componentName){
+    loadScript(getComponentJs(componentName));
+    loadCss(getComponentCss(componentName));
+}
+
+function loadCss(url){
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.href = url;
+    document.head.append(link);
+}
+
+function loadScript(url){
+    var script = document.createElement('script');
+    script.src = url;
+    document.body.append(script);
+}
+
+function getComponentHtml(componentName){
+    return getComponentFile(componentName, "html");
+}
+
+function getComponentCss(componentName){
+    return getComponentFile(componentName, "css");
+}
+
+function getComponentJs(componentName){
+    return getComponentFile(componentName, "js");
+}
+
+function getComponentFile(componentName, extension){
+    return `${ getComponentFolder(componentName) }/${componentName}.${extension}`;
+}
+
+function getComponentFolder(componentName){
+    return `./components/${componentName}`;
+}
+
+
