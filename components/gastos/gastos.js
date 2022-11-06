e13f76de62afeedab1e19cde8937177794ba528e
Regis de Jesus <regisrev@gmail.com>
2022-11-06 09:11:39 -0300

    formatado tabela componente Gastos

--- components/gastos/gastos.js
+++ components/gastos/gastos.js
@@ -1,47 +1,51 @@
 createComponent("gastos",(componetInst,staticContent)=>{
     componetInst.dataArray = [
         {
-            name: "gastos fixos",
+            name: "Fixos",
             value: 1111,
             debts:222
         },
         {
-            name: "gastos fixos",
+            name: "Variavéis",
             value: 333,
             debts:444
         },
         {
-            name: "gastos fixos",
+            name: "Inpostos",
             value: 555,
             debts:4444
         }];
         
-        const firstItem = componetInst.dataArray[0];  
-        
         componetInst.render = () => { 
-            const valores = Object.values(firstItem);
+            //  const valores = Object.values(firstItem);
             // limpa o corpo da tabela
             const tbody = componetInst.querySelector("table tbody");
             tbody.innerHTML = "";
-            const tr = document.createElement("tr");
-            tbody.append(tr);
             
-            valores.forEach(item => {
-                
-                
-                const itemLista = document.createElement("td");
-                itemLista.textContent = item;
-                tr.append(itemLista);
-            })   
+            
+            componetInst.dataArray.forEach(item => {
+                // const correntItem = item;
+                const valores = Object.values(item);
+                const tr = document.createElement("tr");
+                tbody.append(tr);
+                valores.forEach(itemB => {
+                    
+                    
+                    const itemLista = document.createElement("td");
+                    itemLista.textContent = itemB;
+                    tr.append(itemLista);
+                })   
+            })  
         }
         
-        
+        console.warn(componetInst.dataArray);  
         componetInst.addItem = (itemGastos) => {
             componetInst.dataArray.push(itemGastos);
             componetInst.render();
             
         }
-        componetInst.addItem(firstItem);
+        //  componetInst.addItem(firstItem);
+        
         // componetInst.addItemList = (itemList) => {
         //         itemList.forEach(item => {
         //             componetInst.addItem(item);
